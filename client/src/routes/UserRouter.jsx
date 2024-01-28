d2c45c96ffe4625440c32ed6064df19746f99f9b
Jasil Shan <jasilshan786@gmail.com>
2024-01-28 21:48:49 +0530

    bug fix

--- client/src/routes/UserRouter.jsx
+++ client/src/routes/UserRouter.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
-import Table from '../components/Table'
+import Table from '../components/Table/Table'
+import Form from '../components/Form/Form'
 
 const UserRouter = () => {
   return (
     <Routes >
+    <Route path='/' element={<Form />} />
     <Route path='/table' element={<Table />} />
     </Routes>
     )
